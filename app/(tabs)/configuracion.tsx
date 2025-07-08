e1ecdb888e85071f81582fd9fd9e8d7e4952fb8b
Irineo Hiriart <irineohiriart@alu.frlp.utn.edu.ar>
2025-07-08 16:39:21 -0300

    Configuracion/ Agrego frontend. Sin logica

--- app/(tabs)/configuracion.tsx
+++ app/(tabs)/configuracion.tsx
@@ -1,22 +1,96 @@
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, StyleSheet, ScrollView, Image } from 'react-native';
 import { Colors } from '../../constants/Colors'; 
+import { ThemedView } from '@/components/ThemedView';
+import { ThemedText } from '@/components/ThemedText';
+import { SettingsItem } from '@/components/SettingsItem';
+import { useThemeColor } from '@/hooks/useThemeColor';
+
 
 export default function ConfiguracionScreen() {
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Pantalla de Configuración</Text>
-    </View>
+     <ScrollView style={{backgroundColor: useThemeColor({}, 'background')}} contentContainerStyle={[styles.container]}>
+      <ThemedView style={styles.container}>
+        <ThemedView style={[styles.profileContainer, { backgroundColor: useThemeColor({}, 'background') }]}>
+          <Image
+            source={{
+              uri: `https://ui-avatars.com/api/?name=${encodeURIComponent('JP')}`,
+            }}
+            style={styles.avatar}
+          />
+          <ThemedText type="title">Juan Perez</ThemedText>
+          <ThemedText style={{ color: Colors.dark.text }}>juanperez123@gmail.com</ThemedText>
+        </ThemedView>
+
+      <View style={styles.section}>
+          <SectionTitle title="Preferencias" />
+          <SettingsItem icon="moon"
+           text= {`Tema: Claro / Oscuro`}/>
+          <SettingsItem icon="lock-closed-outline" text="Cambiar contraseña" />
+          <SettingsItem icon="language" text="Idioma: Español" />
+        </View>
+
+      <View style={styles.section}>
+          <SectionTitle title="Soporte y cuenta" />
+          <SettingsItem icon="help-circle" text="Centro de ayuda" />
+          <SettingsItem icon="mail" text="Contacto" />
+          <SettingsItem icon="log-out" text="Cerrar sesión"/>
+        </View>
+        </ThemedView>
+    </ScrollView>
+  );
+}
+
+function SectionTitle({ title }: { title: string }) {
+  const textColor = useThemeColor({}, 'text');
+  return (
+    <ThemedText style={[styles.sectionTitle, { color: textColor }]}>{title}</ThemedText>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { 
-    flex: 1, 
-    justifyContent: 'center', 
-    alignItems: 'center',
-    backgroundColor: Colors.light.background,
+  container: {
+    flex: 1,
+    paddingTop: 20,
+    paddingHorizontal: 24,
+    paddingBottom: 32,
+    gap: 32,
   },
-  text: {
-    color: Colors.light.text,
+  profileContainer: {
+    alignItems: 'center',
+    padding: 16,
+    borderRadius: 12,
+    gap: 12,
+    shadowColor: '#000',
+    shadowOpacity: 0.05,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    elevation: 2,
+  },
+  avatar: {
+    width: 72,
+    height: 72,
+    borderRadius: 36,
+  },
+  section: {
+    gap: 16,
+  },
+  sectionTitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 4,
+    opacity: 0.8,
+  },
+  item: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 8,
+  },
+  itemIcon: {
+    marginRight: 12,
+  },
+  itemText: {
+    fontSize: 16,
   },
 });
